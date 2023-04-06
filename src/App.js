6f9ccca074dc358eb51916d4b9a6a72d5a8c4ac5
Nowfel <amahfuj.cse@gmail.com>
2023-04-06 15:51:01 +0600

    hero and pricing component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import Index from "./Components/Index";
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Support from "./Components/Support";
+import Pricing from "./Components/Pricing";
+import AllInOne from "./Components/AllInOne";
 
 function App() {
   return (
     <>
-      <Index></Index>
+      <Navbar />
+      <Hero />
+      <About />
+      <Support />
+      <AllInOne />
+      <Pricing />
+      <Footer />
     </>
   );
 }
