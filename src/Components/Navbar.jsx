f3f3763a8e25cd179c8aa335f0c4029599b09f2f
Nowfel <amahfuj.cse@gmail.com>
2023-04-06 14:53:14 +0600

    nav and custom css added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,68 +1,54 @@
-import React from "react";
-import logo from "../assets/logo.svg";
+import React, {useState} from 'react';
+import { Link, animateScroll as scroll, } from 'react-scroll'
 
-function Navbar() {
+
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+
+    const handleClose =()=> setNav(!nav)
+
   return (
-    <>
-      <nav className="relative container mx-auto p-6">
-        {/* Flex container  */}
-        <div className="flex items-center justify-between">
-          {/* Logo */}
-          <div className="pt-2">
-            <img src={logo} alt="" />
-          </div>
-          <div className="hidden space-x-6 md:flex">
-            <a href="/" className="hover:text-darkGrayishBlue">
-              Pricing
-            </a>
-            <a href="/" className="hover:text-darkGrayishBlue">
-              Product
-            </a>
-            <a href="/" className="hover:text-darkGrayishBlue">
-              About Us
-            </a>
-            <a href="/" className="hover:text-darkGrayishBlue">
-              Careers
-            </a>
-            <a href="/" className="hover:text-darkGrayishBlue">
-              Community
-            </a>
-          </div>
-          {/* <!-- Button --> */}
-          <a
-            href="/"
-            className="hidden p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight md:block"
-          >
-            Get Started
-          </a>
-
-          {/* <!-- Hamburger Icon --> */}
-          <button
-            id="menu-btn"
-            className="block hamburger md:hidden focus:outline-none"
-          >
-            <span className="hamburger-top"></span>
-            <span className="hamburger-middle"></span>
-            <span className="hamburger-bottom"></span>
+    <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
+      <div className='px-2 flex justify-between items-center w-full h-full'>
+        <div className='flex items-center'>
+          <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>BRAND.</h1>
+          <ul className='hidden md:flex'>
+          <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
+          <li><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
+          <li><Link to="support" smooth={true} offset={-50} duration={500}>Support</Link></li>
+          <li><Link to="platforms" smooth={true} offset={-100} duration={500}>Platforms</Link></li>
+          <li><Link to="pricing" smooth={true} offset={-50} duration={500}>Pricing</Link></li>
+          </ul>
+        </div>
+        <div className='hidden md:flex pr-4'>
+          <button className='border-none bg-transparent text-black mr-4'>
+            Sign In
           </button>
+          <button className='px-8 py-3'>Sign Up</button>
         </div>
+        <div className='md:hidden mr-4' onClick={handleClick}>
+            {!nav ? <MenuIcon className='w-5' /> : <XIcon className='w-5' />}
+          
+        </div>
+      </div>
 
-        {/* <!-- Mobile Menu --> */}
-        <div className="md:hidden">
-          <div
-            id="menu"
-            className="absolute flex-col items-center hidden self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md"
-          >
-            <a href="/">Pricing</a>
-            <a href="/">Product</a>
-            <a href="/">About Us</a>
-            <a href="/">Careers</a>
-            <a href="/">Community</a>
-          </div>
+      <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
+          <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="home" smooth={true} duration={500}>Home</Link></li>
+          <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
+          <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="support" smooth={true} offset={-50} duration={500}>Support</Link></li>
+          <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="platforms" smooth={true} offset={-100} duration={500}>Platforms</Link></li>
+          <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="pricing" smooth={true} offset={-50} duration={500}>Pricing</Link></li>
+
+        <div className='flex flex-col my-4'>
+            <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>Sign In</button>
+            <button className='px-8 py-3'>Sign Up</button>
         </div>
-      </nav>
-    </>
+      </ul>
+    </div>
   );
-}
+};
 
 export default Navbar;
