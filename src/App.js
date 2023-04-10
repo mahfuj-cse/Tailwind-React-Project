53999d6ce616a5c966dbc65da84f77039513af58
Nowfel <amahfuj.cse@gmail.com>
2023-04-10 11:44:11 +0600

    file removed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import Navbar from "./Components/Navbar";
-import Hero from "./Components/Hero";
-import Footer from "./Components/Footer";
-import About from "./Components/About";
-import Support from "./Components/Support";
-import Pricing from "./Components/Pricing";
-import AllInOne from "./Components/AllInOne";
+
 
 function App() {
   return (
     <>
-      <Navbar />
-      {/* <Hero />
-      <About />
-      <Support />
-      <AllInOne />
-      <Pricing />
-      <Footer /> */}
+      
     </>
   );
 }
